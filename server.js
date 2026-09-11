cc7ae71c8ff0a49c8362f5f6e08a4309a293f448
sayyadshakil077-maker <sayyadshakil077@gmail.com>
2026-09-11 18:50:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -197,7 +197,7 @@ app.post("/api/login", async (req, res) => {
 // START SERVER
 // ===============================
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
 
     console.log(
         `LMS Backend running at http://localhost:${PORT}`
